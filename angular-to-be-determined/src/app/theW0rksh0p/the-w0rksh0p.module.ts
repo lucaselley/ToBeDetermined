e5ff14701580814da8b3fd2443b66cd6bf1c8cd8
vict2167 <victor.adam.moeller@gmail.com>
2023-04-12 17:49:16 +0200

    added carousel

--- angular-to-be-determined/src/app/theW0rksh0p/the-w0rksh0p.module.ts
+++ angular-to-be-determined/src/app/theW0rksh0p/the-w0rksh0p.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { TheW0rksh0pComponent } from './the-w0rksh0p.component';
 import { TheW0rksh0pRoutingModule } from './the-w0rksh0p-routing.module';
-import { FrontPageComponent } from './frontpage/front-page/front-page.component';
+import { FrontPageComponent } from './frontpage/front-page.component';
 import { W0rkSh0pHeaderComponent } from './header/w0rk-sh0p-header.component';
 import { MaterialModule } from '../shared/material.module';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { CarouselComponent } from './frontpage/carousel/carousel.component';
+import { CommonModule } from '@angular/common';
 
 
 
@@ -15,12 +17,14 @@ import { ProjectsComponent } from './projects/projects.component';
     FrontPageComponent,
     W0rkSh0pHeaderComponent,
     AboutUsComponent,
-    ProjectsComponent
+    ProjectsComponent,
+    CarouselComponent,
+    
   ],
   imports: [
     TheW0rksh0pRoutingModule,
     MaterialModule,
-
+    CommonModule
   ]
 })
 export class TheW0rksh0pModule { }
