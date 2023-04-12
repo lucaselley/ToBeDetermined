7445b1f017c1d82d58baf8cb712b159db5e22ddf
Lucaselley <Lucas_elley8@live.dk>
2023-04-12 13:33:02 +0200

    Nav bar

---
+++ angular-to-be-determined/src/app/theW0rksh0p/header/w0rk-sh0p-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'w0rk-sh0p-header',
+  templateUrl: './w0rk-sh0p-header.component.html',
+  styleUrls: ['./w0rk-sh0p-header.component.css']
+})
+export class W0rkSh0pHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
